533386635bac52a04d26bdb92e44b9cfa3bc4d1d
Katende Nicholas <57605587+Katznicho@users.noreply.github.com>
2024-01-13 10:39:23 +0300

    feat:started on working user verification

--- src/screens/ProfileScreens/VerificationScreen.tsx
+++ src/screens/ProfileScreens/VerificationScreen.tsx
@@ -1,11 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
 import React from 'react'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import { generalStyles } from '../utils/generatStyles'
 
 const VerificationScreen = () => {
     return (
-        <View>
-            <Text>VerificationScreen</Text>
-        </View>
+        <KeyboardAwareScrollView
+            style={[{ flex: 1, width: '100%' }, generalStyles.ScreenContainer]}
+            keyboardShouldPersistTaps="always"
+        >
+            <ScrollView
+                contentContainerStyle={{
+                    margin: 20,
+                }}
+                keyboardShouldPersistTaps="always"
+            >
+                <View style={{ marginHorizontal: 10, marginVertical: 10 }}>
+                    <Text style={[{ fontSize: 20 }, generalStyles.textStyle]}>
+                        Verify Your Account
+                    </Text>
+                </View>
+                <View style={{ marginHorizontal: 10, marginVertical: 10 }}>
+                    <Text style={[generalStyles.textStyle]}>
+                        Verify your account to use all the features
+                    </Text>
+                </View>
+
+            </ScrollView>
+
+        </KeyboardAwareScrollView>
     )
 }
 
